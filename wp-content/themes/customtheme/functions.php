b1802e22a03ef87d3e9ec320e23faf9b407af990
Atul <chambyalatul34@gmail.com>
2025-08-24 21:30:16 +0530

    changes
    changes

--- wp-content/themes/customtheme/functions.php
+++ wp-content/themes/customtheme/functions.php
@@ -4,4 +4,16 @@
  * 
  * @package leelaHoldings
  */
+
+
+
+
+function leelaHoldings_enqueue_scripts(){
+    wp_enqueue_style('main-css', get_template_directory_uri() . '/main.css', ['stylesheet']);
+    wp_enqueue_style('stylesheet' get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css', 'all') );
+}
+
+add_action ('wp_enqueue_scripts', 'leelaHoldings_enqueue_scripts' );
+
+
 ?>

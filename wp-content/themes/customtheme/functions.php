a16934ca37c2d9b772b663d3bef8d907d837cb69
Atul <chambyalatul34@gmail.com>
2025-08-25 07:21:13 +0530

    some
    some

--- wp-content/themes/customtheme/functions.php
+++ wp-content/themes/customtheme/functions.php
@@ -9,7 +9,7 @@
 
 function leelaHoldings_enqueue_scripts() {
     wp_enqueue_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all' );
-    wp_enqueue_script( 'main-js', get_templat_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
+    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
 }
 add_action( 'wp_enqueue_scripts', 'leelaHoldings_enqueue_scripts');
 ?>

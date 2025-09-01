eb9c401996aaac0ac3b98ee3458d983e25c2d50d
Atul <chambyalatul34@gmail.com>
2025-09-01 06:54:53 +0530

    nav bar changes
    nav bar changes

--- wp-content/themes/customtheme/template-parts/header/nav.php
+++ wp-content/themes/customtheme/template-parts/header/nav.php
@@ -7,7 +7,7 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="row w-100">
-    <div class="col-lg-6 d-flex justify-space-between">
+    <div class="col-lg-6 d-flex justify-content-between">
     <a class="navbar-brand" href="#"><img src="/wp-content/uploads/2025/09/logo.png" width="100px" alt=""></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -33,7 +33,7 @@
 
     <!-- // For Mobile  -->
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav d-lg-none">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Tours</a>
         </li>

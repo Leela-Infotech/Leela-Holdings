e1fac287b89d04d191362b8534bf393774ac595c
Atul <chambyalatul34@gmail.com>
2025-09-01 06:51:15 +0530

    new changes

--- wp-content/themes/customtheme/template-parts/header/nav.php
+++ wp-content/themes/customtheme/template-parts/header/nav.php
@@ -6,13 +6,15 @@
 ?>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
+  <div class="row w-100">
+    <div class="col-lg-6 d-flex justify-space-between">
     <a class="navbar-brand" href="#"><img src="/wp-content/uploads/2025/09/logo.png" width="100px" alt=""></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+</div>
     <!-- For Desktop  -->
-    <div class="d-lg-flex justify-content-end d-md-none" id="navbarNav">
+    <div class="col-lg-6 d-lg-flex justify-content-end d-none" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Tours</a>
@@ -30,7 +32,7 @@
     </div>
 
     <!-- // For Mobile  -->
-    <div class="collapse navbar-collapse d-lg-flex justify-content-end d-md-none" id="navbarNav">
+    <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Tours</a>

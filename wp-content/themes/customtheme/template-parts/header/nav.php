efeb359d1cad2993a76116ba87b18a303d0f4071
Atul <chambyalatul34@gmail.com>
2025-09-03 22:16:54 +0530

    new changes
    new changes

--- wp-content/themes/customtheme/template-parts/header/nav.php
+++ wp-content/themes/customtheme/template-parts/header/nav.php
@@ -7,7 +7,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container">
     <!-- Logo -->
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="https://leelaholdings.in/">
       <img src="/wp-content/uploads/2025/09/logo.png" width="100px" alt="">
     </a>
 
@@ -30,38 +30,3 @@
   </div>
 </nav>
 
-    <!-- For Desktop 
-    <div class="col-lg-6 d-lg-flex justify-content-end d-none" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Tours</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-disabled="true">Reviews</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" aria-disabled="true">Contact</a>
-        </li>
-      </ul>
-    </div>
-
-    // For Mobile 
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav d-lg-none">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Tours</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-disabled="true">Reviews</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" aria-disabled="true">Contact</a>
-        </li>
-      </ul>
-</div> -->

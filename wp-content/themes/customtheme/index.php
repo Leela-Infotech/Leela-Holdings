910136fc8cf14258e0172606aaa204a4a43ac1fc
Atul <chambyalatul34@gmail.com>
2025-09-04 04:54:57 +0530

    new changes
    new changes

--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -7,19 +7,32 @@
 get_header();
 ?>
 
-<div class="content p-5">
-    Content
+<div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+    <?php if( have_posts() ){
+        ?>
+         <div class="container">
+            <?php
+            if(is_home() && !is_front_page() ){
+            ?>
+            <header class="mb-5">
+                <h1 class="page-title screen-reader-text">
+                    <?php single_post_title(); ?>
+                </h1>
+
+            </header>
+        <?php
+            }
+         while ( have_posts() ) : the_post();  
+         the_title();
+         the_content();
+    endwhile;
+    }
+    ?>
+    </div>
+    </main>
 </div>
 
-<?php
-trait single{
-    public function single(){
-    echo "Hello world";
-    }
-}
-$base = new base();
-$base->single;
-?>
 
 <?php
 get_footer();

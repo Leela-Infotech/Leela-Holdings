40e265879ae33350dd2855340e8f9b07886d55ac
Atul <chambyalatul34@gmail.com>
2025-09-04 01:09:46 +0530

    new changes
    new changes

--- wp-content/themes/customtheme/functions.php
+++ wp-content/themes/customtheme/functions.php
@@ -42,6 +42,57 @@ function custom_theme_main_menu(){
 add_action('init', 'custom_theme_main_menu');
 
 
+// Enqueue Swiper styles and scripts
+function mytheme_enqueue_swiper_assets() {
+    // Theme stylesheet (style.css)
+    wp_enqueue_style(
+        'mytheme-style',
+        get_stylesheet_uri(),
+        array(),
+        wp_get_theme()->get('Version')
+    );
+
+    // Swiper CSS
+    wp_enqueue_style(
+        'swiper-css',
+        'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+        array(),
+        '11.0.0'
+    );
+
+    // Swiper JS
+    wp_enqueue_script(
+        'swiper-js',
+        'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+        array(),
+        '11.0.0',
+        true
+    );
+
+    // Custom Swiper initialization
+    wp_add_inline_script(
+        'swiper-js',
+        'var swiper = new Swiper(".mySwiper", {
+            spaceBetween: 30,
+            centeredSlides: true,
+            autoplay: {
+              delay: 3500,
+              disableOnInteraction: false,
+            },
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+        });'
+    );
+}
+add_action('wp_enqueue_scripts', 'mytheme_enqueue_swiper_assets');
+
+
 
 
 

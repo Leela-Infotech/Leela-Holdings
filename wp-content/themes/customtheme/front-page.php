f401e746bb058b61b3ef26d3b64485ec56645ed5
Atul <chambyalatul34@gmail.com>
2025-09-05 23:30:03 +0530

    Homepage
    Homepage template

--- wp-content/themes/customtheme/front-page.php
+++ wp-content/themes/customtheme/front-page.php
@@ -8,11 +8,7 @@ get_header();
 ?>
 <main id="main" class="site-main">
     <?php
-      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(188);
-      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(244);
-      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(228);
-      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(222);
-      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(214);
+      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(311);
     ?>
 </main>
 

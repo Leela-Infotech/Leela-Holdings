204c786aba68bab9176f5d8628a5c716a8581636
Atul <chambyalatul34@gmail.com>
2025-09-05 07:49:11 +0530

    new changes
    new changes

--- wp-content/themes/customtheme/front-page.php
+++ wp-content/themes/customtheme/front-page.php
@@ -8,11 +8,10 @@ get_header();
 ?>
 <main id="main" class="site-main">
     <?php
-    if ( have_posts() ) :
-        while ( have_posts() ) : the_post();
-            the_content(); // shows content from the WordPress editor
-        endwhile;
-    endif;
+      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(244);
+      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(228);
+      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(222);
+      echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display(214);
     ?>
 </main>
 

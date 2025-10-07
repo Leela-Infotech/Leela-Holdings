6fc5b1ffb37faad61c19d26e80e523ef9e62ef31
Atul <chambyalatul34@gmail.com>
2025-10-07 19:55:23 +0530

    site logo + title changes

--- wp-content/themes/Leela Theme/header.php
+++ wp-content/themes/Leela Theme/header.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>

dfb0531fa61b4f2f4a408056b2ed74ed4dadbb05
Atul Chambyal <127113053+Chambyal999@users.noreply.github.com>
2025-12-08 22:48:57 +0530

    Update our-staff-section.php

--- wp-content/themes/Leela Theme/template-parts/our-staff-section.php
+++ wp-content/themes/Leela Theme/template-parts/our-staff-section.php
@@ -10,7 +10,7 @@
       [
         'name' => 'Atul Chambyal',
         'designation' => 'Founder & Visionary',
-        'image' => 'https://leelaholdings.in/wp-content/uploads/2025/12/Atul-img12223.png',
+        'image' => 'https://leelaholdings.in/wp-content/uploads/2025/12/atul-img.jpg',
         'desc' => 'Atul laid the foundation of Leela Holdings with the belief that businesses should inspire, innovate, and make lives better.'
       ],
       [

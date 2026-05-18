78ae03c85724e2ff2e265798d190b6d37985075b
Atul <chambyalatul34@gmail.com>
2026-05-18 10:03:10 +0530

    New Template + Knack Cta created

---
+++ wp-content/themes/Leela Theme/template-parts/CTA/c.php
@@ -0,0 +1,7 @@
+<section class="services-container text-center py-5 text-white">
+  <div class="container">
+    <h2 class="mb-3">CheatSheet?</h2>
+    <p class="mb-4">CheatSheet for <b>Calling</b> and <b>Denials</b> Scenarios</p>
+    <a href="https://leelaholdings.in/wp-content/uploads/2026/05/Knack.xlsx" class="btn btn-primary btn-lg">Download</a>
+  </div>
+</section>

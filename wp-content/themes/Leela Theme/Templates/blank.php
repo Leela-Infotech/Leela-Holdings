1308280dc4d5244d080b89ab449e502c73ab202b
Atul <chambyalatul34@gmail.com>
2026-07-20 14:28:00 +0530

    new blank template

---
+++ wp-content/themes/Leela Theme/Templates/blank.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: Blank Template
+ * @package Leela Infotech
+ */
+
+get_header(); ?>
+
+<main id="primary" class="site-main">
+
+<img src="https://leelaholdings.in/wp-content/uploads/2026/07/img1.jpg" alt="main">
+
+    <div class="container py-5">
+        <?php
+        while ( have_posts() ) :
+            the_post();
+            the_content();
+        endwhile;
+        ?>
+    </div>
+
+    <!-- Services Section -->
+    <?php 
+     get_template_part('template-parts/CTA/c');
+     get_template_part('template-parts/services_infotech');
+
+      ?>
+</main>
+
+<?php
+get_footer();
